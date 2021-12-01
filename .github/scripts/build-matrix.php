54c00de2a5af212a60ce9d1be9807c2c31f5b0e9
BluePsyduck <bluepsyduck@gmx.com>
2021-12-01 21:51:01 +0100

    Debugging the script.

--- .github/scripts/build-matrix.php
+++ .github/scripts/build-matrix.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 $versions = explode(' ', getenv('PHP_VERSIONS'));
 $suites = [];
 
+var_dump(__DIR__, file_exists('../vendor/bin/phpunit'), file_exists('vendor/bin/phpunit'), $versions);die;
+
 if (file_exists('../vendor/bin/phpunit')) {
     $output = shell_exec('../vendor/bin/phpunit --list-suites');
     if (preg_match_all('#^ - (.*)$#m', $output, $matches)) {
