7d3184d8226c866267193168b8f02c51ddb58470
BluePsyduck <bluepsyduck@gmx.com>
2021-12-01 21:33:33 +0100

    Use env and swap matrix values.

--- .github/scripts/build-matrix.php
+++ .github/scripts/build-matrix.php
@@ -2,32 +2,23 @@
 
 declare(strict_types=1);
 
-$versions = explode(' ', '8.1 8.0 7.4');
+$versions = explode(' ', getenv('PHP_VERSIONS'));
 $suites = [];
 
-//if (file_exists('vendor/bin/phpunit')) {
-//    $output = shell_exec('vendor/bin/phpunit --list-suites');
-
-    $output = <<<EOT
-PHPUnit 9.5.10 by Sebastian Bergmann and contributors.
-
-Available test suite(s):
- - unit-test
- - serializer-test
-EOT;
-
+if (file_exists('vendor/bin/phpunit')) {
+    $output = shell_exec('vendor/bin/phpunit --list-suites');
     if (preg_match_all('#^ - (.*)$#m', $output, $matches)) {
         $suites = array_map('trim', $matches[1]);
     }
-//}
+}
 
 $matrix = [];
 if (count($versions) > 0 && count($suites) > 0) {
     foreach ($versions as $version) {
         foreach ($suites as $suite) {
             $matrix[] = [
-                'version' => $version,
                 'suite' => $suite,
+                'version' => $version,
                 'cover' => $suite === 'unit-test',
             ];
         }
